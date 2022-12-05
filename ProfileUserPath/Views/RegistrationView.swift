7bc7b9da05f97520e0dd9f48434728943b79712a
Matthew Molinar <matthew.a.molinar@gmail.com>
2022-12-05 06:40:19 -0600

    implemented image picker

--- ProfileUserPath/Views/RegistrationView.swift
+++ ProfileUserPath/Views/RegistrationView.swift
@@ -14,11 +14,35 @@ struct RegistrationView: View {
     @State var password: String = ""
     @State var username: String = ""
     @State var name: String = ""
+    @State var imagePickerShowing = false
+    @State var image = UIImage()
 
     @EnvironmentObject var viewModel: AuthViewModel
     
+   
     var body: some View {
         Form {
+            Button(action: {
+                imagePickerShowing.toggle()
+                
+            })
+            
+            {
+                ZStack {
+                    HStack {
+                        Image("batman")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 140, height: 140)
+                            .clipShape(Circle())
+                        Text("Upload your profile picture here")
+                    }
+                    
+                }
+            }
+            .sheet(isPresented: $imagePickerShowing) {
+                ImagePicker(sourceType: .photoLibrary, selectedImage: $image)
+            }
             Section {
                 TextField("Email", text: $email)
                 TextField("Password", text: $password)
@@ -27,8 +51,9 @@ struct RegistrationView: View {
 
                 Button("Sign Up") {
                     viewModel.registerUser(email: email, password: password, username: username, name: name)
-                    
                 }
+                
+                
             }
         }
     }
